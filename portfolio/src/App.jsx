9b1091fcdbf17b736dc588148572e97ec9626457
mirzobekrozmetov <mirzobekrozmetov066@gmail.com>
2026-05-14 17:33:08 +0500

    yangi

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -4,8 +4,10 @@ import './App.css'; // CSS faylni ulashni unutmang!
 import ContactMen from './Contact.jsx'
 import QuoteApp from './QuoteApp';
 import Men from './men';
+import Appi from './api.jsx';
+import CurrencyConventor from './Convertor.jsx';
 
-
+import MovieSearch from './kino.jsx';
 
 function App() {
   const Home = () => <h2 className="page-title">Men haqimda</h2>;
@@ -19,16 +21,22 @@ function App() {
         <Link to="/about" className="nav-link">Men haqimda </Link>
         <Link to="/contact" className="nav-link">Aloqa</Link>
         <Link to="/hikmatlar" className="nav-link">Quote</Link>
+        <Link to="/api" className="nav-link">api</Link>
+        <Link to="/Convertor" className="nav-link">Pul</Link>
+        <Link to="/kino" className="nav-link">Kino</Link>
       </nav>
 
-      
+
       <div className="content-area">
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="/about" element={<Men/>} />
+          <Route path="/about" element={<Men />} />
           <Route path="/contact" element={<ContactMen />} />
-          <Route path="/hikmatlar" element={<QuoteApp/>} />
-
+          <Route path="/hikmatlar" element={<QuoteApp />} />
+          <Route path="/api" element={<Appi />} />
+          <Route path="/Convertor" element={<CurrencyConventor />} />
+               <Route path="/kino" element={<MovieSearch />} />
+          
         </Routes>
       </div>
     </BrowserRouter>
